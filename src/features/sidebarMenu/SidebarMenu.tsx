efe0565009f062e2e83510b7c2d500fb8eccf25b
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-26 14:54:22 +0200

    sidebar menu

--- src/features/sidebarMenu/SidebarMenu.tsx
+++ src/features/sidebarMenu/SidebarMenu.tsx
@@ -8,10 +8,10 @@ import { getSession } from "@/components/utils/CacheSession"
 export const SidebarMenu = async () => {
   const session = await getSession()
 
-  return <div className="w-[230px] py-2 h-screen flex flex-col justify-between">
-    <div className="flex flex-col">
-        <Link href="/">
-            <Image src="/x-logo.jpg" alt="logo" width={50} height={50} className="rounded-full" />
+  return <aside className="w-[240px] py-2 sticky top-0 h-screen flex flex-col justify-between">
+    <div className="flex flex-col space-y-5">
+        <Link href="/" className="p-2 rounded-full hover:bg-gray-200/10 w-fit duration-75">
+            <Image src="/x-logo.jpg" alt="logo" width={50} height={50} className="rounded-full" priority/>
         </Link>
 
         <Links />
@@ -20,5 +20,5 @@ export const SidebarMenu = async () => {
 
 
     <Profile session={session?.user}/>
-  </div>
+  </aside>
 }
