6a3ed3e7fd431fd0765202b68cf8907d43021674
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-26 15:04:11 +0200

    get posts

--- src/server/Posts.ts
+++ src/server/Posts.ts
@@ -4,6 +4,7 @@ import {z} from "zod"
 import { authenticatedAction } from "@/lib/safe-actions"
 import { extractHashtags } from "@/components/utils/extractHashtags"
 import { revalidatePath } from "next/cache"
+import { cache } from 'react'
 
 // Create new Post
 export const createPost = authenticatedAction
@@ -30,3 +31,42 @@ export const createPost = authenticatedAction
         revalidatePath("/")
         return post
     })
+
+// Récupère tous les posts mais fait en apparaitre que 10 par page
+export const getPosts = cache(async (page: number, pageSize: number = 10) => {
+    const [posts, totalPosts] = await Promise.all([
+        prisma.post.findMany({
+            select: {
+                id: true,
+                content: true,
+                image: true,
+                createdAt: true,
+                author: {
+                    select: {
+                        id: true,
+                        name: true,
+                        image: true,
+                    }
+                },
+                _count: {
+                    select: {
+                        likes: true,
+                        comments: true,
+                    }
+                }
+            },
+            take: pageSize,
+            skip: (page - 1) * pageSize,
+            orderBy: {
+                createdAt: "desc"
+            }
+        }),
+        prisma.post.count()
+    ])
+
+    return {
+        posts,
+        totalPages: Math.ceil(totalPosts / pageSize),
+        currentPage: page,
+    }
+})
