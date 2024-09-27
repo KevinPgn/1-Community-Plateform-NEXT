5abe81e469b0fdc5f1f458f730ac2855d22b58ef
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-27 23:43:39 +0200

    type

--- src/components/formCreatePost/FormCreatePost.tsx
+++ src/components/formCreatePost/FormCreatePost.tsx
@@ -8,7 +8,8 @@ import { Button } from "../ui/button"
 import { Image as ImageIcon } from "lucide-react"
 import {ToastContainer, toast} from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-
+import { UploadDropzone } from "@uploadthing/react"
+import type { OurFileRouter } from "@/app/api/uploadthing/core"
 interface FormCreatePostProps {
   userImage: string | null | undefined
   userId: string | null | undefined
@@ -17,15 +18,18 @@ interface FormCreatePostProps {
 export const FormCreatePost = ({userImage, userId}: FormCreatePostProps) => {
   const router = useRouter()
   const {register, handleSubmit, watch, formState: {errors}} = useForm()
+  const [isActive, setIsActive] = React.useState(false)
+  const [imageUrl, setImageUrl] = React.useState<string | null>(null)
   const characterLimit = 280
   const content = watch("content", "")
   const remainingCharacters = characterLimit - (content?.length || 0)
 
   const onSubmit = handleSubmit(async (data) => {
     try {
-      await createPost(data);
+      await createPost({...data, imageUrl});
       router.refresh();
       toast.success("Post created successfully");
+      setImageUrl(null)
     } catch (error) {
       toast.error("Error creating post");
     }
@@ -57,7 +61,9 @@ export const FormCreatePost = ({userImage, userId}: FormCreatePostProps) => {
 
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center gap-3">
-            <ImageIcon className="w-5 h-5 text-blue-500 cursor-pointer" />
+            <ImageIcon
+            onClick={() => setIsActive(!isActive)}
+            className="w-5 h-5 text-blue-500 cursor-pointer" />
             <span
               className={`text-sm ${
                 remainingCharacters < 20 ? "text-red-500" : "text-gray-500"
@@ -77,6 +83,23 @@ export const FormCreatePost = ({userImage, userId}: FormCreatePostProps) => {
       </div>
 
       <ToastContainer />
+      {isActive && (
+        <UploadDropzone<OurFileRouter, "imageUploader">
+          endpoint="imageUploader"
+          onClientUploadComplete={(res: any) => {
+            // Vérifiez le type exact de 'res' ici
+            console.log(res);
+            if (res && res[0] && 'url' in res[0]) {
+              setImageUrl(res[0].url);
+              toast.success("Image téléchargée avec succès");
+              setIsActive(false);
+            }
+          }}
+          onUploadError={(error: Error) => {
+            toast.error(`Erreur lors du téléchargement de l'image : ${error.message}`);
+          }}
+        />
+      )}
     </form>
   );
 }
