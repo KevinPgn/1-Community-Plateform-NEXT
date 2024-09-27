b32ccffa035b40af8c644198f6eee2501c398035
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-27 13:47:18 +0200

    suggestions users

--- src/features/sidebarRight/SidebarRight.tsx
+++ src/features/sidebarRight/SidebarRight.tsx
@@ -1,9 +1,11 @@
 import { SearchBar } from "./SearchBar"
 import { SubscribeYou } from "./SubscribeYou"
+import { SuggestionsUsers } from "./SuggestionsUsers"
 
 export const SidebarRight = () => {
   return <div className="w-[350px] flex flex-col gap-5 py-3 h-screen sticky top-0">
     <SearchBar />
     <SubscribeYou />
+    <SuggestionsUsers />
   </div>
 }
