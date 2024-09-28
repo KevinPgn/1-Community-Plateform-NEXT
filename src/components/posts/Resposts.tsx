1533760c5e4c180ff24d7bb38cdb4cbc0b9d56a8
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-28 22:16:15 +0200

    fill

--- src/components/posts/Resposts.tsx
+++ src/components/posts/Resposts.tsx
@@ -32,7 +32,7 @@ export const Reposts = ({ repostsCount, isReposted, postId }: RepostsProps) => {
   return <div
     onClick={handleRepost}
     className="flex items-center cursor-pointer gap-1 text-sm text-white/50 group">
-    <Repeat className={`w-5 h-5 ${optimisticReposts.isReposted ? "fill-green-500 text-green-500" : "text-white/50"} duration-75`} />
+    <Repeat className={`w-5 h-5 ${optimisticReposts.isReposted ? "text-green-500" : "text-white/50 group-hover:text-green-500"} duration-75`} />
     {repostsCount}
   </div>
 }
