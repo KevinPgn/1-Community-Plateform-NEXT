e334bd84c814a04243afd002ecfbbdbdd522aadf
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-28 18:31:43 +0200

    like

--- src/components/posts/Likes.tsx
+++ src/components/posts/Likes.tsx
@@ -7,7 +7,7 @@ export const Likes = ({ likesCount, isLiked, postId }: { likesCount: number, isL
     <div
     onClick={() => likePost({postId})}
     className="flex items-center cursor-pointer gap-1 text-sm text-white/50 group">
-      <Heart className={`w-5 h-5 ${isLiked ? "fill-red-500 text-red-500" : "fill-white/50 group-hover:fill-red-500 group-hover:text-red-500 duration-75"}`} />
+      <Heart className={`w-5 h-5 ${isLiked ? "fill-red-500 text-red-500" : "group-hover:fill-red-500 group-hover:text-red-500 duration-75"}`} />
       {likesCount}
     </div>
   </>
