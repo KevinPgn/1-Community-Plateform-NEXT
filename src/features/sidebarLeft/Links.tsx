fcd4461e8deba467b7419c00319017bc7c1ab769
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-29 14:32:09 +0200

    links

--- src/features/sidebarLeft/Links.tsx
+++ src/features/sidebarLeft/Links.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Link from "next/link"
-import { Compass, MessageSquare, User, Image as ImageIcon, Settings, Bell } from "lucide-react"
+import { Compass, MessageSquare, User, Image as ImageIcon, Settings, Bell, Badge } from "lucide-react"
 import { usePathname } from "next/navigation"
 import {cn} from "@/lib/utils"
 
@@ -12,27 +12,41 @@ export const Links = () => {
       <span>Home Feed</span>
     </Link>
 
-    <Link href="/" className="flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75">
-      <MessageSquare size={21} />
-      <span>Messages</span>
+    <Link href="/" className={cn("flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75", pathname === "/messages" && "bg-white text-black font-semibold")}>
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-3">
+          <MessageSquare size={21} />
+          <span>Messages</span>
+        </div>
+        <div className="w-5 h-5 bg-red-500 rounded-full flex items-center justify-center">
+          <span className="text-white text-xs">1</span>
+        </div>
+      </div>  
     </Link>
 
-    <Link href="/" className="flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75">
+    <Link href="/" className={cn("flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75", pathname === "/profile" && "bg-white text-black font-semibold")}>
       <User size={21} />
       <span>Profile</span>
     </Link>
 
-    <Link href="/" className="flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75">
-      <Bell size={21} />
-      <span>Notifications</span>
+    <Link href="/" className={cn("flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75", pathname === "/notifications" && "bg-white text-black font-semibold")}>
+    <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-3">
+          <Bell size={21} />
+          <span>Notifications</span>
+        </div>
+        <div className="w-5 h-5 bg-red-500 rounded-full flex items-center justify-center">
+          <span className="text-white text-xs">10</span>
+        </div>
+      </div> 
     </Link>
 
-    <Link href="/" className="flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75">
+    <Link href="/" className={cn("flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75", pathname === "/media" && "bg-white text-black font-semibold")}>
       <ImageIcon size={21} />
       <span>Media</span>
     </Link>
 
-    <Link href="/" className="flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75">
+    <Link href="/" className={cn("flex items-center gap-3 p-3 px-5 rounded-xl hover:bg-white hover:text-black hover:font-semibold duration-75", pathname === "/settings" && "bg-white text-black font-semibold")}>
       <Settings size={21} />
       <span>Settings</span>
     </Link>
