351a3fd92439b45d70f0b4337ebd6deece6da517
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-29 14:29:39 +0200

    create post btn

---
+++ src/features/sidebarLeft/ButtonCreatePost.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+
+export const ButtonCreatePost = () => {
+  return <Button variant="ghost" className="w-[90%] border border-white/20 flex items-center gap-2 p-3 py-5 rounded-md mx-auto">
+    <Plus size={21} />
+    <span>Create Post</span>
+  </Button>
+}
