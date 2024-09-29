6082c24f3c50ed067d222655fb9b5ca642cef216
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-30 00:38:16 +0200

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { SidebarRight } from "@/features/sidebarRight/SidebarRight";
+
 export default async function Home() {
   return (
-    <>
-    
-    </>
+    <div className="flex flex-1">
+      <div className="flex flex-1 p-5 h-screen bg-blue-900 max-w-[1400px] mx-auto">
+
+      </div>
+
+      <SidebarRight />
+    </div>
   );
 }
---
+++ src/features/sidebarRight/SidebarRight.tsx
@@ -0,0 +1,5 @@
+export const SidebarRight = () => {
+  return <aside className="h-screen w-[280px] flex flex-col justify-between pb-10 max-md:w-[100px] sticky top-0 rounded-tr-lg rounded-br-lg bg-white dark:bg-[#0b0b0b]">
+    
+  </aside>
+}
