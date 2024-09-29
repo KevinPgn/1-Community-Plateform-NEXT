30fbcd84a9ce4a42b3ecb140b764312e73f8f6d9
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-30 01:15:20 +0200

    récupération searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { HomeFeeds } from "@/components/homeFeeds/HomeFeeds";
 import { SidebarRight } from "@/features/sidebarRight/SidebarRight";
 
-export default async function Home() {
+export default async function Home({searchParams}: {searchParams: {filter: string}}) {
   return (
     <div className="flex flex-1 justify-center">
       <div className="flex-1 p-5 max-w-[1000px] mx-auto">
