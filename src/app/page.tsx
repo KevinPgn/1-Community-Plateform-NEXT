fc2dc471109ec6b3ee6d90d2812df6cb95d251fd
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-30 00:38:32 +0200

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { SidebarRight } from "@/features/sidebarRight/SidebarRight";
 export default async function Home() {
   return (
     <div className="flex flex-1">
-      <div className="flex flex-1 p-5 h-screen bg-blue-900 max-w-[1400px] mx-auto">
+      <div className="flex flex-1 max-w-[1400px] mx-auto">
 
       </div>
 
