6eaf9db4c197f0c9440caa9248eb5cd85a43a091
TeruBW <kevin.pigeon2810@gmail.com>
2024-09-30 01:18:09 +0200

    posts

--- src/features/sidebarRight/SidebarRight.tsx
+++ src/features/sidebarRight/SidebarRight.tsx
@@ -2,7 +2,7 @@ import { SuggestionsUser } from "./SuggestionsUser"
 import { Tendances } from "./Tendances"
 
 export const SidebarRight = () => {
-  return <aside className="h-screen overflow-y-auto flex flex-col p-3 w-[350px] pb-10 max-xl:hidden sticky top-0 rounded-tr-lg rounded-br-lg bg-white dark:bg-[#0b0b0b]">
+  return <aside className="h-screen overflow-y-auto flex flex-col p-3 w-[350px] pb-10 max-xl:hidden sticky top-0 rounded-tr-lg rounded-br-lg bg-white dark:bg-[#141414]">
     <SuggestionsUser />
     <Tendances />
   </aside>
