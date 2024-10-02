d9a5acd72544d734b5d73d8908d403df1be15c95
TeruBW <kevin.pigeon2810@gmail.com>
2024-10-03 00:38:29 +0200

    page profile

---
+++ src/components/profilePage/EnTete.tsx
@@ -0,0 +1,5 @@
+export const EnTete = () => {
+  return (
+    <div></div>
+  )
+}
---
+++ src/server/Profile.action.ts
@@ -0,0 +1,28 @@
+"use server"
+import prisma from "@/lib/prisma"
+import {z} from "zod"
+import { authenticatedAction } from "@/lib/safe-actions"
+import { extractHashtags } from "@/components/utils/extractHashtags"
+import { cache } from "react"
+
+export const getProfile = cache(async (userId: string) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId
+        },
+        select: {
+            id: true,
+            name: true,
+            pseudo: true,
+            image: true,
+            bio: true,
+            banner: true,
+            createdAt: true,
+            isVerified: true,
+            website: true,
+            location: true,
+        }
+    })
+
+    return user
+})
